fae77cb6a93f26a75e53e33aacef769bc2d0c9c1
José Díaz <josediazrrnn@gmail.com>
2025-06-22 15:16:31 +0200

    kill console programatically

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-#![windows_subsystem = "windows"]
-
 #[tokio::main]
 async fn main() {
     boquilahub::cli::run_cli().await;
+    hide_window();
     boquilahub::gui::run_gui();
+}
+
+#[cfg(target_os = "windows")]
+fn hide_window() {
+    use winapi::um::wincon::FreeConsole;
+    unsafe {
+        FreeConsole(); // Detaches and closes the console window
+    }
 }
