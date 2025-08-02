40d212a5468d1dbb9fbae61e9cad475a4f151d06
Reim-developer <contact.kaxtr@gmail.com>
2025-08-02 11:55:50 +0700

    Added cli handler to `main`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use boquilahub::cli::{run_cli, Cli};
+use clap::Parser;
+
 #[tokio::main]
 async fn main() {
-    boquilahub::cli::run_cli().await;
-    #[cfg(all(windows, not(debug_assertions)))]
-    hide_window();
-    boquilahub::gui::run_gui();
+    let cli = Cli::try_parse();
+
+    match cli {
+        Ok(cli) => {
+            if cli.command.is_none() {
+                #[cfg(all(windows, not(debug_assertions)))]
+                hide_window();
+                boquilahub::gui::run_gui();
+
+                return;
+            }
+
+            run_cli(cli.command.expect("Could not run CLI")).await;
+        }
+        Err(error) => error.exit(),
+    }
 }
 
 #[cfg(all(windows, not(debug_assertions)))]
