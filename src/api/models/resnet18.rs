f8ee9eafe88f37442d359fd162a9e75fbd415b5a
José Díaz <josediazrrnn@gmail.com>
2026-04-26 02:03:42 +0200

    mockup resnet18

--- src/api/models/resnet18.rs
+++ src/api/models/resnet18.rs
@@ -14,17 +14,77 @@ use ort::{session::Session, value::ValueType};
 
 pub struct ResNet18 {
     pub classes: Vec<String>,
-    pub batch_size: i32,
-    pub channel: u32, // 3, RGB or similar
-    pub input_width: u32,
-    pub input_height: u32,
+
+    // Input Tensor
+    pub batch_size: i32,   // number of windows/clips
+    pub channel: u32,      //  number of channel, 1 for single channel mel spectrogram
+    pub input_height: u32, // number of mel freq bins
+    pub input_width: i32,  // time steps, time frames, (width of the spectrogram)
     pub input_name: String,
-    pub output_width: u32,
+    // Output Tensor
+    pub output_width: i32,
     pub output_height: u32,
     pub output_name: String,
     pub task: Task,
     pub post_processing: Vec<PostProcessing>,
     pub session: Session,
     pub config: ModelConfig,
-    pub input_format: TensorFormat,
+}
+
+impl ModelTrait for ResNet18 {
+    fn new(
+        classes: Vec<String>,
+        task: Task,
+        post_processing: Vec<PostProcessing>,
+        session: Session,
+        config: ModelConfig,
+    ) -> Self {
+        let (batch_size, channel, input_height, input_width) = match &session.inputs[0].input_type {
+            ValueType::Tensor { dimensions, .. } => (
+                dimensions[0] as i32,
+                dimensions[1] as u32,
+                dimensions[2] as u32,
+                dimensions[3] as i32,
+            ),
+            _ => {
+                panic!("Not supported");
+            }
+        };
+
+        let input_name = session.inputs[0].name.clone();
+
+        let (output_width, output_height) = match &session.outputs[0].output_type {
+            ValueType::Tensor { dimensions, .. } => (dimensions[0] as i32, dimensions[1] as u32),
+            _ => {
+                panic!("Not supported");
+            }
+        };
+
+        let output_name: String = session.outputs[0].name.clone();
+
+        ResNet18 {
+            classes,
+            batch_size,
+            channel,
+            input_width,
+            input_height,
+            input_name,
+            output_width,
+            output_height,
+            output_name,
+            task,
+            post_processing,
+            session,
+            config,
+        }
+    }
+
+    fn run(&self, img: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> AIOutputs {
+        let probs = ProbSpace {
+            classes: vec![],
+            probs: vec![],
+            classes_ids: vec![],
+        };
+        return AIOutputs::Classification(probs);
+    }
 }
