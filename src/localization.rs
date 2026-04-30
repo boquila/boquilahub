c744b60ee278024011984f94198fb40ce2725310
José Díaz <josediazrrnn@gmail.com>
2026-04-30 21:19:02 +0200

    localizaiion cleanup

--- src/localization.rs
+++ src/localization.rs
@@ -13,8 +13,14 @@ pub enum Lang {
 impl Default for Lang {
     fn default() -> Self {
         let locale = sys_locale::get_locale().unwrap_or_else(|| "en-US".to_owned());
-        let lang_code = locale.get(0..2).unwrap_or("en").to_lowercase();
-        match lang_code.as_str() {
+        let lang_code = locale.get(0..2).unwrap_or("en");
+        Self::from_str(lang_code)
+    }
+}
+
+impl Lang {
+    pub fn from_str(s: &str) -> Self {
+        match s.to_lowercase().as_str() {
             "en" => Lang::EN,
             "es" => Lang::ES,
             "fr" => Lang::FR,
@@ -26,22 +32,10 @@ impl Default for Lang {
             _ => Lang::EN,
         }
     }
-}
 
-impl Lang {
     pub fn from_optional_str(s: Option<&str>) -> Self {
         match s {
-            Some(code) => match code.to_lowercase().as_str() {
-                "en" => Lang::EN,
-                "es" => Lang::ES,
-                "fr" => Lang::FR,
-                "de" => Lang::DE,
-                "zh" => Lang::ZH,
-                "ja" => Lang::JA,
-                "pt" => Lang::PT,
-                "vi" => Lang::VI,
-                _ => Lang::default(),
-            },
+            Some(code) => Self::from_str(code),
             None => Lang::default(),
         }
     }
