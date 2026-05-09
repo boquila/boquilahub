e62dec80f5db2241f76f6a6b37448da3a294f99b
José Díaz <josediazrrnn@gmail.com>
2026-05-09 22:06:41 +0200

    moved audio tests

--- src/api/audio.rs
+++ src/api/audio.rs
@@ -246,34 +246,3 @@ impl AudioData {
             })
     }
 }
-
-#[test]
-fn smoke() {
-    let a = AudioData::from_file("assets/test/audio.mp3").unwrap();
-    let (min, max, rms) = a.amplitude_stats();
-
-    println!(
-        "samples={} rate={} channels={} duration={:.3}s",
-        a.samples.len(),
-        a.sample_rate,
-        a.channels,
-        a.duration()
-    );
-    println!("amp min={:.4} max={:.4} rms={:.4}", min, max, rms);
-    println!("dc_offset={:.6}", a.dc_offset());
-    println!("preview={:?}", a.preview(16));
-}
-
-#[test]
-fn audio_inference() {
-    let audio = AudioData::from_file("assets/test/bird.mp3").unwrap();
-    let _ = super::bq::GlobalBQ::First.set_model(
-        "models/MD_AudioBirds_V1.bq",
-        super::ep::Ep::Cpu,
-        None,
-    );
-    
-    let aioutput = crate::api::bq::process_audio(&audio);
-    println!("Inference success",);
-    println!("AI Outputs: {:?}",aioutput);
-}
