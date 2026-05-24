97fec45419018bd82583c3336a4226625950eb29
José Díaz <josediazrrnn@gmail.com>
2026-05-24 23:38:57 +0200

    removed mkv

--- src/api/formats.rs
+++ src/api/formats.rs
@@ -3,8 +3,8 @@ pub const IMAGE_FORMATS: [&'static str; 23] = [
     "png", "pnm", "pbm", "pgm", "ppm", "qoi", "tga", "tiff", "tif", "webp",
 ];
 
-pub const VIDEO_FORMATS: [&'static str; 35] = [
-    "mp4", "m4v", "mkv", "avi", "mov", "qt", "wmv", "flv", "f4v", "webm", "mpg", "mpeg", "mpe",
+pub const VIDEO_FORMATS: [&'static str; 34] = [
+    "mp4", "m4v", "avi", "mov", "qt", "wmv", "flv", "f4v", "webm", "mpg", "mpeg", "mpe",
     "m1v", "m2v", "3gp", "3g2", "ts", "mts", "m2ts", "mxf", "vob", "asf", "rm", "rmvb", "ogv",
     "ogg", "divx", "swf", "wtv", "dvr-ms", "amv", "hevc", "h265", "h264",
 ];
