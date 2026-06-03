6b30d3dcd3495a67108f19e751da5d87456919dd
José Díaz <josediazrrnn@gmail.com>
2026-06-03 19:03:44 +0200

    add core type

--- src/api/abstractions.rs
+++ src/api/abstractions.rs
@@ -74,6 +74,14 @@ pub struct XYXY {
     pub class_id: u32,
 }
 
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+pub struct XY {
+    pub x: f32,
+    pub y: f32,
+    pub prob: f32,
+    pub class_id: u32,
+}
+
 impl XYXY {
     pub fn new(x1: f32, y1: f32, x2: f32, y2: f32, prob: f32, class_id: u32) -> Self {
         Self {x1,y1,x2,y2,prob,class_id}
@@ -450,6 +458,12 @@ pub struct XYXYc {
     pub extra_cls: Option<Vec<Prob>>,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct XYc {
+    pub xy: XY,
+    pub label: String,
+}
+
 impl XYXYc {
     pub fn new(xyxy: XYXY, label: String) -> Self {
         XYXYc {xyxy, label, extra_cls: None,}
@@ -470,6 +484,7 @@ pub enum AIOutputs {
     Segmentation(Vec<SEGc>),
     AudioClassification(Vec<AudioProb>),
     Embed(Embedding),
+    PointDetection(Vec<XYc>)
 }
 
 impl AIOutputs {
@@ -480,6 +495,7 @@ impl AIOutputs {
             AIOutputs::Segmentation(segments) => segments.is_empty(),
             AIOutputs::AudioClassification(audio_probs) => audio_probs.is_empty(),
             AIOutputs::Embed(emb) => emb.values.is_empty(),
+            AIOutputs::PointDetection(points) => points.is_empty(),
         }
     }
 
