cabdd346bd1e29e4ebfc9f8cd24f0bf0ff57beb5
José Díaz <jose@boquila.org>
2026-06-23 04:01:23 +0200

    delete time measure

--- src/api/processing/inference.rs
+++ src/api/processing/inference.rs
@@ -6,10 +6,8 @@ pub fn inference<'a>(
     input: &'a Array<f32, Ix4>,
     input_name: &str,
 ) -> anyhow::Result<ort::session::SessionOutputs<'a>> {
-    let start = std::time::Instant::now();
     #[allow(mutable_transmutes)]
     let session: &'a mut ort::session::Session = unsafe { std::mem::transmute(session) };
-    println!("Elapsed: {:?}", start.elapsed());
     let input = ort::value::TensorRef::from_array_view(input.view()).map_err(ort_err)?;
     Ok(session.run(ort::inputs![input_name => input]).map_err(ort_err)?)
 }
