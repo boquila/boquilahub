09b96f23b29941ebb9d8acbef26e748531de343c
José Díaz <josediazrrnn@gmail.com>
2026-08-23 19:59:59 +0200

    Tiny fixes (cli errors, gui sizing, roll up efficiency) (#61)
    * change min size to an usable one
    
    * delete unecessary line (path gets created at write time)
    
    * performance improvmeent in geofencing

--- src/api/stream.rs
+++ src/api/stream.rs
@@ -17,7 +17,6 @@ impl Feed {
         // Initialize FFmpeg
         ffmpeg::init()?;
         ffmpeg::util::log::set_level(ffmpeg::util::log::Level::Quiet);
-        std::fs::create_dir_all("export/feed").unwrap();
 
         // Open the RTSP stream with options for better RTSP handling
         let mut opts: ffmpeg::Dictionary<'_> = ffmpeg::Dictionary::new();
